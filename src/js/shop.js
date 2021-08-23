72a09752a24169425e18ec1d433374a5a86c8e92
Gabriel <ziemacki25@gmail.com>
2021-08-23 20:15:05 +0200

    plspls

--- src/js/shop.js
+++ src/js/shop.js
@@ -1,8 +1,33 @@
 var valuetext = document.getElementById("range-value")
 var getvalue = document.getElementById("slider-range")
 
+var valuetext2 = document.getElementById("range-value2")
+var getvalue2 = document.getElementById("slider-range2")
+
 const shownumber = () => {
     valuetext.textContent = getvalue.value + "zł"
 }
 
+const shownumber2 = () => {
+    valuetext2.textContent = getvalue2.value + "zł"
+}
+
 getvalue.addEventListener('mousemove', shownumber)
+getvalue2.addEventListener('mousemove', shownumber2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
